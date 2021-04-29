d547cd346576a294c8786efd58d37c8770bc9f40
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-04-29 11:20:48 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		string s;
+		cin >> s;
+		int zeroes = 0;
+		int ones = 0;
+		for (int i = 0; i < (int) s.size(); i++) {
+			if (s[i] == '0') {
+				// count the number of 0s
+				zeroes++;
+			} else {
+				// count the number of 1s
+				ones++;
+			}
+		}
+		// find the minimum between 'zeroes' and 'ones', and
+		// if it's odd, the Alice wins, otherwise she loses
+		cout << (min(zeroes, ones) % 2 == 1 ? "DA" : "NET") << '\n';
+	}
+	return 0;
+}
